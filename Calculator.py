996a5902b91ade12959871ad920a621f832d1288
Manya Khede <68687797+manya5@users.noreply.github.com>
2024-11-21 23:15:16 +0530

    Basic Calculator

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+a = 78
+b = 58 
+
+print("The value of ",a, "+" ,3, "is:", a + b)
+print("The value of ",a, "-" ,3, "is:", a - b)
+print("The value of ",a, "*" ,3, "is:", a * b)
+print("The value of ",a, "%" ,3, "is:", a % b)
+print("The value of ",a, "**" ,3, "is:", a ** b)
+
+
