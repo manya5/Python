4544e96331bdade5dc8440e19b55584a232ac7b3
Manya Khede <68687797+manya5@users.noreply.github.com>
2024-11-23 00:01:23 +0530

    Day Second

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Wellcome to the BandNameGenerator")
+city = input("Which city did you grew in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be: " + city + " " + pet)
--- Calculator.py
+++ Calculator.py
@@ -6,5 +6,3 @@ print("The value of ",a, "-" ,3, "is:", a - b)
 print("The value of ",a, "*" ,3, "is:", a * b)
 print("The value of ",a, "%" ,3, "is:", a % b)
 print("The value of ",a, "**" ,3, "is:", a ** b)
-
-
