175bd63b79169c8d9385848f1c3e2b21bfae58f6
Manya Khede <68687797+manya5@users.noreply.github.com>
2024-11-23 00:36:44 +0530

    DAY 2

---
+++ Typecasting.py
@@ -0,0 +1,6 @@
+name_of_the_user = input ("Enter the name")
+length_of_name = len(name_of_the_user)
+
+print("Number of letters in your name: ")
+print("length_of_name")
+print("Number of letters in your name:" + str(length_of_name))
