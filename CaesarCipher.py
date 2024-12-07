77b3367a68c3c74e87ded34eb48df9c27a67260d
Manya Khede <68687797+manya5@users.noreply.github.com>
2024-12-07 23:59:22 +0530

    day10

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -4,7 +4,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text,shift_amount):
+'''def encrypt(original_text,shift_amount):
     cipher_text = ""
 
     for letter in original_text:
@@ -14,4 +14,32 @@ def encrypt(original_text,shift_amount):
         cipher_text +=alphabet[shifted_position]
     print(f"Here is the encoded result:{cipher_text}")
 
-encrypt(original_text=text, shift_amount=shift)
+encrypt(original_text=text, shift_amount=shift)'''
+
+def caesar(original_text,shift_amount,encode_or_decode):
+    output_text = " "
+
+    for letter in original_text:
+
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+        shifted_position= alphabet.index(letter) + shift_amount
+
+        shifted_position %= len(alphabet)
+        output_text +=alphabet[shifted_position]
+    print(f"Here is the encoded result:{output_text}")
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+
+'''def decrypt(original_text,shift_amount):
+    output_text = " "
+
+    for letter in original_text:
+        shifted_position= alphabet.index(letter) - shift_amount
+
+        shifted_position %= len(alphabet)
+        cipher_text +=alphabet[shifted_position]
+    print(f"Here is the encoded result:{output_text}")
+
+decrypt(original_text=text, shift_amount=shift)'''
